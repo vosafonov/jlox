62435b80f8c247d4855822e859af020ba4181ddd
vosafonov <v.o.safonov.94@gmail.com>
2017-12-03 17:01:21 +0700

    [+] два режима выполения кода: 1) из файла, 2) prompt

--- src/com/lox/Lox.java
+++ src/com/lox/Lox.java
@@ -1,6 +1,11 @@
 package com.lox;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
 public class Lox {
     public static void main(String[] args) throws IOException
@@ -14,11 +19,24 @@ public class Lox {
         }
     }
 
-    private static void runFile(String fileName)
+    private static void runFile(String path) throws IOException
     {
+        byte[] bytes = Files.readAllBytes(Paths.get(path));
+        run(new String(bytes, Charset.defaultCharset()));
     }
 
-    private static void runPrompt()
+    private static void runPrompt() throws IOException
+    {
+        InputStreamReader input = new InputStreamReader(System.in);
+        BufferedReader reader = new BufferedReader(input);
+
+        while(true) {
+            System.out.print("> ");
+            run(reader.readLine());
+        }
+    }
+
+    private static void run(String source)
     {
     }
 }
