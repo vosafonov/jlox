73004ae2203f55c9c8deca70d8bc813b8e006595
vosafonov <v.o.safonov.94@gmail.com>
2017-12-12 06:04:21 +0700

    [+] вспомогательный класс AstPrinter

--- src/com/lox/Token.java
+++ src/com/lox/Token.java
@@ -15,7 +15,7 @@ class Token {
     }
 
     private final TokenType type;
-    private final String lexeme;
+    final String lexeme;
     private final Object literal;
     private final int line;
 }
