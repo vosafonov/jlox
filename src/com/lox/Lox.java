1a57f0651726b1d412e92c2e3e06433450c424bc
vosafonov <v.o.safonov.94@gmail.com>
2018-01-06 01:24:46 +0700

    [+] более информативные сообщения об ошибках

--- src/com/lox/Lox.java
+++ src/com/lox/Lox.java
@@ -68,21 +68,21 @@ public class Lox {
     static void error(Token token, String message)
     {
         if (token.type == TokenType.EOF) {
-            report(token.line, " at end", message);
+            report(token.line, "at end", message);
         } else {
-            report(token.line, " at '" + token.lexeme + "'", message);
+            report(token.line, "at '" + token.lexeme + "'", message);
         }
     }
 
     static void runtimeError(RuntimeError error)
     {
-        System.err.println("[line " + error.token.line + "] " + error.getMessage());
+        System.err.println("[line " + error.token.line + "] Runtime Error: " + error.getMessage());
         hadRuntimeError = true;
     }
 
     private static void report(int line, String where, String message)
     {
-        System.err.println("[line " + line + "] Error" + where + ": " + message);
+        System.err.println("[line " + line + "] Syntax Error " + where + ": " + message);
         hadError = true;
     }
 
